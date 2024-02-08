314a6bc5cf618ab74ddaa068d207c88c69664e2a
clifrone <liviu.copot@gmail.com>
2024-02-08 17:06:46 +0100

    Delete run_container.sh

--- run_container.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -e AWS_ACCESS_KEY_ID="AKIAU5YMQWRQT34EY54D" -e AWS_SECRET_ACCESS_KEY="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" -e  AWS_DEFAULT_REGION=eu-west-1 licop_load_data_from_aws
