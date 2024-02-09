8ac45a885f3ee87c8ac45819dab3a1682a37656a
Copot Liviu <liviu.copot@gmail.com>
2024-02-09 11:28:10 +0000

    some generic dates

--- snowflake/licop_workspace.sql
+++ snowflake/licop_workspace.sql
@@ -64,13 +64,87 @@ daily_max as
         parameter, 
         year_month_day,
         daily_max_value,
-        max(daily_max_value) over (partition by location, parameter order by year_month_day rows between 6 preceding and 1 following row) last_6_days_max
+        max(daily_max_value) over (partition by location, parameter order by year_month_day rows between 6 preceding and 1 following ) last_6_days_max
 
     from daily_max
     
 )
-select * from daily_max_month
+select * from daily_max_month, 
 order by location, parameter, year_month_day;
 
 
+WITH CTE_MY_DATE AS (
+
+  SELECT DATEADD(DAY, -1*SEQ4(), CURRENT_DATE()) AS MY_DATE
+
+    FROM TABLE(GENERATOR(ROWCOUNT => (365*10) )) -- Number of days after reference date in previous line
+
+ ) SELECT * FROM CTE_MY_DATE order by 1 desc;
+
+ select current_date , seq4(), dateadd(day, seq4())
+ from table(generator(rowcount=>10));
     
+
+//Compare the daily max of each metric to its 7-day historic maximum. You will require window functions
+with 
+cte_all_days AS (
+
+  SELECT DATEADD(DAY, -1*SEQ4(), CURRENT_DATE()) AS each_day
+
+    FROM TABLE(GENERATOR(ROWCOUNT => (365*10) )) -- Number of days after reference date in previous line
+
+ ),
+ cte_days_with_data as 
+(
+    select  
+        location,
+       date_trunc("minute",utc_date::TIMESTAMP) utc_day,
+        //year(utc_date::TIMESTAMP) ||'/'|| lpad(month(utc_date::TIMESTAMP),2,0)||'/'|| lpad(day(utc_date::TIMESTAMP),2,0) year_month_day,
+        parameter,
+        value
+    from 
+        raw_open_aq_data
+    group by ALL
+    order by 1,2,3
+)
+select each_day, utc_day,  location,parameter,value
+    from   
+        cte_all_days a left join cte_days_with_data d
+    on a.each_day=d.utc_day
+where a.each_day between 
+                    (select min(utc_date::TIMESTAMP) from raw_open_aq_data)
+                    and
+                    (select max(utc_date::TIMESTAMP) from raw_open_aq_data)
+order by each_day desc;
+
+//2021-01-02 01:00:00.000	2021-07-22 08:03:59.000
+
+select min(utc_date::TIMESTAMP),max(utc_date::TIMESTAMP) from raw_open_aq_data;
+
+daily_max as 
+(
+    select  
+        location,
+        year(utc_date::TIMESTAMP) ||'/'|| lpad(month(utc_date::TIMESTAMP),2,0)||'/'|| lpad(day(utc_date::TIMESTAMP),2,0) year_month_day,
+        parameter,  
+        count(1), 
+        max(value) daily_max_value
+    from 
+        raw_open_aq_data
+    group by ALL
+    order by 1,2,3
+),
+ daily_max_month as 
+(
+    select 
+        location, 
+        parameter, 
+        year_month_day,
+        daily_max_value,
+        max(daily_max_value) over (partition by location, parameter order by year_month_day rows between 6 preceding and 1 following ) last_6_days_max
+
+    from daily_max
+    
+)
+select * from daily_max_month, 
+order by location, parameter, year_month_day;
